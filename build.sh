00bc83a33b39f8984725cb34a83549b7a9971329
i7-8700 <1281261856@qq.com>
2020-07-23 15:30:22 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+mkdir -p build
+cd build
+
+CC=g++
+CFLAGS=`pkg-config --cflags --libs libevent`
+
+$name=000.discard
+$CC ../${name}/${name}.cpp $CFLAGS -o $name 
+
+
+$name=001.daytime
+$CC ../${name}/${name}.cpp $CFLAGS -o $name 
+
+
+$name=002.time
+$CC ../${name}/${name}.cpp $CFLAGS -o $name 
+
+
+$name=002.time_client
+$CC ../${name}/${name}.cpp $CFLAGS -o $name 
+
