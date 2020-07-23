528726952c2a5d344ee6e673b775b76bc0c628c5
i7-8700 <1281261856@qq.com>
2020-07-23 21:21:52 +0800

    update

--- 002.time_client_libevent/002.time_client_libevent.cpp
+++ 002.time_client_libevent/002.time_client_libevent.cpp
@@ -65,6 +65,7 @@ When used via TCP the time service works as follows:
 
 #include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <event2/listener.h>
